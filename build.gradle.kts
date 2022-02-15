b1d03f2c5d0d3c9be24c1e39c2d6c7b003ed0c56
m.nikiforov@just-ai.com (JAICP) <m.nikiforov@just-ai.com>
2022-02-15 07:36:09 +0000

    Create project from template: jaicp-cailapub-ru

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,10 +7,11 @@ plugins {
 group = "com.justai.jaicf"
 version = "1.0.0"
 
-val jaicf = "1.2.0"
+val jaicf = "1.2.2"
 val logback = "1.2.3"
+val ktor = "1.5.1"
 
-// Main class to run application on heroku. Either JaicpPollerKt, or JaicpServerKt. Will propagate to .jar main class.
+// Main class to run application on JAICP Cloud. Either JaicpPollerKt, or JaicpServerKt. Will propagate to .jar main class.
 application {
     mainClassName = "com.justai.jaicf.template.connections.JaicpServerKt"
 }
@@ -28,6 +29,7 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback")
 
     implementation("com.just-ai.jaicf:core:$jaicf")
+    implementation("com.just-ai.jaicf:mongo:$jaicf")
     implementation("com.just-ai.jaicf:jaicp:$jaicf")
     implementation("com.just-ai.jaicf:caila:$jaicf")
 }
@@ -39,15 +41,4 @@ tasks {
     compileTestKotlin {
         kotlinOptions.jvmTarget = "1.8"
     }
-    shadowJar {
-        archiveFileName.set("app.jar")
-    }
-}
-
-tasks.create("stage") {
-    dependsOn("shadowJar")
-}
-
-tasks.withType<com.justai.jaicf.plugins.jaicp.build.JaicpBuild> {
-    mainClassName.set(application.mainClassName)
 }
